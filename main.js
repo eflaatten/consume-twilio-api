02da36fe80b2c2fd7dc8f87e485906fdee3214f2
eflaatten <elliotflaatten@live.com>
2024-06-28 13:40:59 -0500

    basic commit

--- main.js
+++ main.js
@@ -1,44 +1,31 @@
 require('dotenv').config()
-
-
-(function (){
   
-  var variables = {}
-  
-  const sendSMS = async (toPhoneNumber, fromPhoneNumber, body) => {
-    const accountSid = process.env.TWILIO_ACCOUNT_SID
-    const authToken = process.env.TWILIO_AUTH_TOKEN
+var variables = {}
+
+const sendSMS = async (accountSid, authToken, toPhoneNumber, fromPhoneNumber, body) => {
+    // const accountSid = process.env.TWILIO_ACCOUNT_SID
+    // const authToken = process.env.TWILIO_AUTH_TOKEN
+
+    const accountSid = variables['accountSid']
+    const authToken = variables['authToken']
+    const toPhoneNumber = variables['toPhoneNumber']
+    const fromPhoneNumber = variables['fromPhoneNumber']
+    const body = variables['body']
+
     const client = require('twilio')(accountSid, authToken)
-    const url = `https://api.twilio.com/2010-04-01/Accounts/${accountSid}/Messages.json`;
-    
-    variables['body'] = body
-    variables['toPhoneNumber'] = toPhoneNumber
-    variables['fromPhoneNumber'] = fromPhoneNumber
-
-    let requestOptions = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
-    }
-
 
     try {
-      let response = await fetch(url, requestOptions)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        return data
-      })
-    } catch (error) {
-      console.log('Error:', error)
+        const message = await client.messages.create({
+            body: body,
+            from: fromPhoneNumber,
+            to: toPhoneNumber
+        })
+        return message
+    } catch (error){
+        console.log('error: ', error)
+        return { errors: error }
     }
+}
 
-  }
+sendSMS()
 
-  return {
-    sendSMS 
-  }
-
-
-})
